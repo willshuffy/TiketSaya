34a9a4b4c11f1b5107309a7c87f7c160639e49a4
will_shuffy <saputrawilly26@gmail.com>
2019-04-09 15:55:03 +0700

    memfungsikan splash screen ke tampilan get started

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/SplashActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/SplashActivity.java
@@ -1,5 +1,7 @@
 package tiketsaya.willdev.com.tiketsaya;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //setting timer untuk 1 detik
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //merubah activity ke activity lain
+                Intent gogetstarted = new Intent(SplashActivity.this,GetStartedActivity.class);
+                startActivity(gogetstarted);
+                finish();
+
+            }
+        }, 1000); //1000 ms = 1s
+
     }
 }
