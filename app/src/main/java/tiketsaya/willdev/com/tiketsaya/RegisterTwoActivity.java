2038c48b7c8a6c7439163d70e193b473b2565a96
will_shuffy <saputrawilly26@gmail.com>
2019-04-14 17:16:32 +0700

    membuat layout succes register

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/RegisterTwoActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/RegisterTwoActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterTwoActivity extends AppCompatActivity {
 
     LinearLayout btn_back;
+    Button btn_continue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,15 @@ public class RegisterTwoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent backtoprev = new Intent(RegisterTwoActivity.this,RegisterOneActivity.class);
                 startActivity(backtoprev);
+            }
+        });
+
+        btn_continue =findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosuccess = new Intent(RegisterTwoActivity.this,SuccessRegisterActivity.class);
+                startActivity(gotosuccess);
             }
         });
     }
