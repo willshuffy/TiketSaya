12fe5dafb29e7ce96ad47ac1a93b7ef78b99fb0f
will_shuffy <saputrawilly26@gmail.com>
2019-04-20 00:35:47 +0700

    membuat layout Home

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/SignInActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/SignInActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
+    Button btn_sign_in;
     TextView btn_create_new_account;
 
     @Override
@@ -22,6 +24,16 @@ public class SignInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotoregisterone = new Intent(SignInActivity.this,RegisterOneActivity.class);
                 startActivity(gotoregisterone);
+            }
+        });
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotohome = new Intent(SignInActivity.this,HomeActivity.class);
+                startActivity(gotohome);
             }
         });
     }
--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/SuccessRegisterActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/SuccessRegisterActivity.java
@@ -1,13 +1,27 @@
 package tiketsaya.willdev.com.tiketsaya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccessRegisterActivity extends AppCompatActivity {
+
+    Button btn_explore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_register);
+
+        btn_explore = findViewById(R.id.btn_explore);
+        btn_explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotohome = new Intent(SuccessRegisterActivity.this,HomeActivity.class);
+                startActivity(gotohome);
+            }
+        });
     }
 }
