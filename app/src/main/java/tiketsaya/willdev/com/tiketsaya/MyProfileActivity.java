7f9bad465307a1611dd8c04ea943e5820bc695e6
will_shuffy <saputrawilly26@gmail.com>
2019-04-20 08:09:35 +0700

    membuat layout tampilan Ticket

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/MyProfileActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/MyProfileActivity.java
@@ -1,13 +1,28 @@
 package tiketsaya.willdev.com.tiketsaya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    LinearLayout item_my_ticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        item_my_ticket = findViewById(R.id.item_my_ticket);
+        item_my_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotomytickets= new Intent(MyProfileActivity.this,MyTicketScreenActivity.class);
+                startActivity(gotomytickets);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/MyTicketScreenActivity.java
@@ -0,0 +1,13 @@
+package tiketsaya.willdev.com.tiketsaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTicketScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ticket_screen);
+    }
+}
