f030b0fa56c62ee0c79eacca305e31e92ae1b810
will_shuffy <saputrawilly26@gmail.com>
2019-04-20 07:28:28 +0700

    membuat layout My Profile

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/HomeActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/HomeActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+
+import com.github.florent37.shapeofview.shapes.CircleView;
 
 public class HomeActivity extends AppCompatActivity {
 
     LinearLayout btn_ticket_pisa;
+    CircleView btn_to_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,16 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotopisaticket = new Intent(HomeActivity.this,TiketDetailActivity.class);
                 startActivity(gotopisaticket);
+            }
+        });
+
+        btn_to_profile = findViewById(R.id.btn_to_profile);
+        btn_to_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoprofile= new Intent(HomeActivity.this,MyProfileActivity.class);
+                startActivity(gotoprofile);
+
             }
         });
     }
