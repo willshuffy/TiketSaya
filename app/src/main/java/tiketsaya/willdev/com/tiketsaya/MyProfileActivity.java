faf665a00d728a29f503cb8f66edc6ed9da5609c
will_shuffy <saputrawilly26@gmail.com>
2019-04-20 08:49:48 +0700

    membuat layout Edit Profile

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/MyProfileActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/MyProfileActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MyProfileActivity extends AppCompatActivity {
 
     LinearLayout item_my_ticket;
+    Button btn_edit_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,16 @@ public class MyProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotomytickets= new Intent(MyProfileActivity.this,MyTicketScreenActivity.class);
                 startActivity(gotomytickets);
+
+            }
+        });
+
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
+        btn_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoeditprofile= new Intent(MyProfileActivity.this,EditProfileActivity.class);
+                startActivity(gotoeditprofile);
 
             }
         });
