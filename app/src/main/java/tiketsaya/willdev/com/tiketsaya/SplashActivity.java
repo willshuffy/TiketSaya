2df30ada92034910e0d00b65aab1e373f45801f7
will_shuffy <saputrawilly26@gmail.com>
2019-04-21 16:38:06 +0700

    membuat animation pada layout splash screen

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/SplashActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/SplashActivity.java
@@ -4,15 +4,35 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation app_splash, btt;
+    ImageView app_logo;
+    TextView app_subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //setting timer untuk 1 detik
+        //load animation
+        app_splash = AnimationUtils.loadAnimation(this,R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this,R.anim.btt);
+
+        //load element
+        app_logo = findViewById(R.id.app_logo);
+        app_subtitle = findViewById(R.id.app_subtitle);
+
+        //run animation
+        app_logo.startAnimation(app_splash);
+        app_subtitle.startAnimation(btt);
+
+        //setting timer untuk 3 detik
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -23,7 +43,7 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
 
             }
-        }, 1000); //1000 ms = 1s
+        }, 3000); //2000 ms = 2s
 
     }
 }
