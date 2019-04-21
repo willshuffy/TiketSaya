095ca3cbecc5c0810017b279aa09bf8014a00d05
will_shuffy <saputrawilly26@gmail.com>
2019-04-21 18:53:25 +0700

    membuat animation pada layout succes register

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/SuccessRegisterActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/SuccessRegisterActivity.java
@@ -4,18 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SuccessRegisterActivity extends AppCompatActivity {
 
     Button btn_explore;
+    Animation app_splash, btt, top_to_bottom;
+    ImageView icon_success;
+    TextView app_title;
+    TextView app_subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_register);
 
+        //load element
         btn_explore = findViewById(R.id.btn_explore);
+        icon_success = findViewById(R.id.icon_success);
+        app_title = findViewById(R.id.app_title);
+        app_subtitle = findViewById(R.id.app_subtitle);
+
+        //load animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+        top_to_bottom = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom);
+
+        //run animation
+        icon_success.startAnimation(app_splash);
+        app_title.startAnimation(top_to_bottom);
+        app_subtitle.startAnimation(top_to_bottom);
+        btn_explore.startAnimation(btt);
+
         btn_explore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
