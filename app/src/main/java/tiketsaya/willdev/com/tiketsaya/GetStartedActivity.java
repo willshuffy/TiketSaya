2b637891097a625ff85663b7968821125094eb9d
will_shuffy <saputrawilly26@gmail.com>
2019-04-21 18:35:08 +0700

    membuat animation pada layout get started

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/GetStartedActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/GetStartedActivity.java
@@ -4,19 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GetStartedActivity extends AppCompatActivity {
 
     Button btn_sign_in, btn_new_account;
+    Animation btt, ttb;
+    ImageView emblem_app;
+    TextView intro_app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
+        //load animation
+        ttb = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        //load element
         btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_new_account = findViewById(R.id.btn_new_account);
+
+        emblem_app = findViewById(R.id.emblemView);
+        intro_app = findViewById(R.id.tex_intro);
+
+        //run animation
+        emblem_app.startAnimation(ttb);
+        intro_app.startAnimation(ttb);
+        btn_sign_in.startAnimation(btt);
+        btn_new_account.startAnimation(btt);
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
