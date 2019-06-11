8c083fb2fc1e12e866d3445796cb6a6d2d47754c
will_shuffy <saputrawilly26@gmail.com>
2019-06-11 22:35:29 +0700

    User register 2 - add photo user to local storage with picasso

--- app/src/main/java/tiketsaya/willdev/com/tiketsaya/RegisterTwoActivity.java
+++ app/src/main/java/tiketsaya/willdev/com/tiketsaya/RegisterTwoActivity.java
@@ -1,16 +1,27 @@
 package tiketsaya.willdev.com.tiketsaya;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.squareup.picasso.Picasso;
 
 public class RegisterTwoActivity extends AppCompatActivity {
 
     LinearLayout btn_back;
-    Button btn_continue;
+    Button btn_continue, btn_add_photo;
+    ImageView pic_photo_register_user;
+
+    Uri photo_location;
+    Integer photo_max = 1;
+
 
 
     @Override
@@ -18,7 +29,29 @@ public class RegisterTwoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_two);
 
+        btn_continue =findViewById(R.id.btn_continue);
         btn_back = findViewById(R.id.btn_back);
+        btn_add_photo = findViewById(R.id.btn_add_photo);
+        pic_photo_register_user = findViewById(R.id.pic_photo_register_user);
+
+
+        btn_add_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPhoto();
+
+            }
+        });
+
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosuccess = new Intent(RegisterTwoActivity.this,SuccessRegisterActivity.class);
+                startActivity(gotosuccess);
+            }
+        });
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,13 +60,31 @@ public class RegisterTwoActivity extends AppCompatActivity {
             }
         });
 
-        btn_continue =findViewById(R.id.btn_continue);
-        btn_continue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent gotosuccess = new Intent(RegisterTwoActivity.this,SuccessRegisterActivity.class);
-                startActivity(gotosuccess);
-            }
-        });
+    }
+
+    String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    public void findPhoto(){
+        Intent pic = new Intent();
+        pic.setType("image/*");
+        pic.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(pic, photo_max);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode ==photo_max && resultCode == RESULT_OK && data != null && data.getData() != null)
+        {
+            photo_location = data.getData();
+            Picasso.with(this).load(photo_location).centerCrop().fit().into(pic_photo_register_user);
+        }
+
     }
 }
